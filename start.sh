3b0c87c7e5fdecdca6cd6a55b304f322b4f7f7e5
Matvei Rukol <banano-palma@inbox.ru>
2025-09-27 09:26:16 +0300

    .

--- start.sh
+++ start.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+# Создаем файл для логов
+touch /var/log/backend.log
+
+# Запускаем бэкенд в фоновом режиме, перенаправляя ВЕСЬ его вывод в этот файл
+/usr/local/bin/games-backend > /var/log/backend.log 2>&1 &
+
+# Даем бэкенду секунду на запуск или падение
+sleep 2
+
 # === ДЛЯ ОТЛАДКИ ===
-echo "--- Printing environment variables ---"
-printenv
-echo "------------------------------------"
+echo "--- Backend Log ---"
+cat /var/log/backend.log
+echo "-------------------"
 # =====================
 
-
-# Запускаем бэкенд-сервер в фоновом режиме
-# Путь соответствует тому, куда мы скопировали файл в Dockerfile
-/usr/local/bin/games-backend &
-
 # Запускаем Nginx на переднем плане
+echo "Starting Nginx..."
 nginx -g 'daemon off;'
